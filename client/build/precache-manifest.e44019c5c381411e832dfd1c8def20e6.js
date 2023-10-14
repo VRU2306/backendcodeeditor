e87531f736c2fa19d2ea917abb839985a338d11b
VRUJESH H M <60779508+VRU2306@users.noreply.github.com>
2023-10-14 10:17:32 +0530

    new dev

---
+++ client/build/precache-manifest.e44019c5c381411e832dfd1c8def20e6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0e5d6c44d3aecaf3da88adf75959ce8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47e75e1bd9d4827e53eb",
+    "url": "/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "896da5849a8ae4e53935",
+    "url": "/static/css/main.b59d62a6.chunk.css"
+  },
+  {
+    "revision": "47e75e1bd9d4827e53eb",
+    "url": "/static/js/2.f5a7c147.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f5a7c147.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "896da5849a8ae4e53935",
+    "url": "/static/js/main.b580a55f.chunk.js"
+  },
+  {
+    "revision": "44d459044b9da8ae1201",
+    "url": "/static/js/runtime-main.6829a894.js"
+  },
+  {
+    "revision": "7b711a98365332aeaa71d2e7d3e366aa",
+    "url": "/static/media/css.7b711a98.png"
+  },
+  {
+    "revision": "2b895aa41129190b0a33da9f6297b3fc",
+    "url": "/static/media/html.2b895aa4.png"
+  },
+  {
+    "revision": "94ae2dcd4fa410811cab4e1fbb403340",
+    "url": "/static/media/js.94ae2dcd.png"
+  }
+]);
